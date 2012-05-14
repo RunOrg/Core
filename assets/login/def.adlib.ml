cdff286a11f8603833c3f21e1ce44b4ff4790dca
Victor Nicollet <victor@nicollet.net>
2012-05-14 20:24:03 +0000

    Asset hierarchy
    git-svn-id: svn://svn.runorg.com/app/retheme@7464 b8684840-d642-4688-b1b8-d9d0b1097da4

---
+++ assets/login/def.adlib.ml
@@ -0,0 +1,4 @@
+| `Login_Title
+| `Login_Form_Login
+| `Login_Form_Password
+| `Login_Form_Submit
---
+++ assets/login/fr.adlib.ml
@@ -0,0 +1,4 @@
+| `Login_Title -> "RunOrg - Connexion"
+| `Login_Form_Login -> "Email"
+| `Login_Form_Password -> "Mot de passe"
+| `Login_Form_Submit -> "Connexion"
