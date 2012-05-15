15498ee840612942cb8a732144ba59fbb20b0292
Victor Nicollet <victor@nicollet.net>
2012-05-15 15:41:30 +0000

    Fix session
    git-svn-id: svn://svn.runorg.com/app/retheme@7478 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cSession.ml
+++ ocaml/cSession.ml
@@ -7,15 +7,15 @@ open BatPervasives
 let session = "R"
 
 let start uid res = 
-  let proof = IUser.make_login_token uid in
+  let proof = IUser.Deduce.make_login_token uid in
   let token = IUser.to_string uid ^ "-" ^ proof in
-  Action.cookie ~name:session ~value:token ~life:0 res
+  Action.with_cookie ~name:session ~value:token ~life:0 res
 
 let check req = 
   let! cookie     = req_or None (req # cookie session) in
   let! uid, proof = nothrow_or None (lazy (BatString.split cookie "-")) in
   let  uid = IUser.of_string uid in
-  IUser.from_login_token proof uid
+  IUser.Deduce.from_login_token proof uid
 
 let close res = 
-  Action.cookie ~name:session ~value:"" ~life:0 res
+  Action.with_cookie ~name:session ~value:"" ~life:0 res
