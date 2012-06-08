0dcf2a10d222b87a76208dcb5b1ac773b4c1bcc9
Victor Nicollet <victor@nicollet.net>
2012-06-08 17:43:44 +0000

    Render tags
    git-svn-id: svn://svn.runorg.com/app/retheme@7584 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cWebsite_about.ml
+++ ocaml/cWebsite_about.ml
@@ -12,8 +12,14 @@ let () = UrlClient.def_about begin fun req res ->
   
   let! profile = ohm_req_or (C404.render cuid res) $ MInstance.Profile.get iid in  
 
+  let tags = List.map (fun tag -> (object
+    method url  = "" 
+    method text = tag
+  end)) (profile # tags) in
+
   let main = Asset_Website_About.render (object
     method html = None (* profile # desc *)
+    method tags = tags
   end) in
 
   let left = Left.render ~calendar:false cuid key iid in 
