e5fb0922f21ecccb4f7de96887f94d7f1863a83b
Victor Nicollet <victor@nicollet.net>
2012-06-21 12:10:52 +0000

    Start displaying list of events
    git-svn-id: svn://svn.runorg.com/app/retheme@7625 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cEvents.ml
+++ ocaml/cEvents.ml
@@ -5,9 +5,21 @@ open Ohm.Universal
 open BatPervasives
 
 let () = CClient.define UrlClient.Events.def_home begin fun access -> 
-  O.Box.fill (Asset_Event_ListPrivate.render (object
-    method list = []
-    method url_new     = "#"
-    method url_options = Some "#" 
-  end))
+  O.Box.fill $ O.decay begin 
+    let! list = ohm $ MEntity.All.get_by_kind access `Event in
+    let! list = ohm $ Run.list_map begin fun entity -> 
+      return (object
+	method coming = 0 
+	method date = None
+	method pic = None
+	method status = "" 
+	method title = "" 
+      end)
+    end list in 
+    Asset_Event_ListPrivate.render (object
+      method list = list
+      method url_new     = "#"
+      method url_options = Some "#" 
+    end) 
+  end
 end
