ae1ae6d3b3dbd4f2d36d52374a2e6c0392d4b4a8
Victor Nicollet <victor@nicollet.net>
2012-06-26 17:20:03 +0000

    Display first few values for entity
    git-svn-id: svn://svn.runorg.com/app/retheme@7643 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cEvents_see.ml
+++ ocaml/cEvents_see.ml
@@ -5,20 +5,33 @@ open Ohm.Universal
 open BatPervasives
 
 let () = CClient.define UrlClient.Events.def_see begin fun access -> 
+
+  let e404 = O.Box.fill (Asset_Client_PageNotFound.render ()) in
+
+  let! eid = O.Box.parse IEntity.seg in
+
+  let! entity = ohm_req_or e404 $ O.decay (MEntity.try_get access eid) in
+  let! entity = ohm_req_or e404 $ O.decay (MEntity.Can.view entity) in
+
   O.Box.fill $ O.decay begin
+
+    let! name = ohm $ CEntityUtil.name entity in
+    let! pic  = ohm $ CEntityUtil.pic_large entity in
+    let! desc = ohm $ CEntityUtil.desc entity in
+    
     Asset_Event_Page.render (object
-      method pic = "" 
-      method navig = []
-      method admin = None
-      method title = ""
+      method pic        = pic
+      method navig      = []
+      method admin      = None
+      method title      = name
       method pic_change = None 
-      method date  = None
-      method status = None
-      method desc = None
-      method time = None
-      method location = None
-      method details = "/"
-      method box = Html.str "O Hai" 
+      method date       = None
+      method status     = None
+      method desc       = desc
+      method time       = None
+      method location   = None
+      method details    = "/"
+      method box        = Html.str "O Hai" 
     end)
   end
 end
