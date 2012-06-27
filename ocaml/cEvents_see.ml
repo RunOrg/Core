5c9df81e22efd3fc676e18e6f8a5838163448464
Victor Nicollet <victor@nicollet.net>
2012-06-27 07:52:20 +0000

    Add more information to entity page
    git-svn-id: svn://svn.runorg.com/app/retheme@7644 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cEvents_see.ml
+++ ocaml/cEvents_see.ml
@@ -15,21 +15,38 @@ let () = CClient.define UrlClient.Events.def_see begin fun access ->
 
   O.Box.fill $ O.decay begin
 
+    let! now  = ohmctx (#time) in
+
+    let  tmpl = MEntity.Get.template entity in 
     let! name = ohm $ CEntityUtil.name entity in
     let! pic  = ohm $ CEntityUtil.pic_large entity in
     let! desc = ohm $ CEntityUtil.desc entity in
+    let  date = BatOption.bind MFmt.float_of_date (MEntity.Get.date entity) in 
+    let! loc  = ohm begin 
+      match PreConfig_Template.Meaning.location tmpl with None -> return None | Some field -> 
+	let! data = ohm $ CEntityUtil.data entity in
+	return (try Some (Json.to_string (List.assoc field data)) with _ -> None)
+    end in 
     
+    let location = 
+      match loc with None -> None | Some addr -> 
+	Some (object
+	  method url  = "http://maps.google.fr/maps?f=q&hl=fr&q="^addr
+	  method name = addr
+	 end)
+    in
+
     Asset_Event_Page.render (object
       method pic        = pic
       method navig      = []
       method admin      = None
       method title      = name
       method pic_change = None 
-      method date       = None
+      method date       = BatOption.map (fun t -> (t,now)) date
       method status     = None
       method desc       = desc
-      method time       = None
-      method location   = None
+      method time       = date
+      method location   = location
       method details    = "/"
       method box        = Html.str "O Hai" 
     end)
