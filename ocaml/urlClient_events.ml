7c852a45b832c3c38c5ec90389269e45ac66d5df
Victor Nicollet <victor@nicollet.net>
2012-06-29 14:14:51 +0000

    Add more endpoints in the event admin section
    git-svn-id: svn://svn.runorg.com/app/retheme@7671 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/urlClient_events.ml
+++ ocaml/urlClient_events.ml
@@ -15,6 +15,8 @@ let see,     def_see     = child def_create "event"
 let admin,   def_admin   = child def_see    "ev/admin"
 let edit,    def_edit    = child def_admin  "ev/edit"
 let picture, def_picture = child def_admin  "ev/picture"
+let people,  def_people  = child def_admin  "ev/people"
+let access,  def_access  = child def_admin  "ev/access"
 
 let tabs = 
   (function 
