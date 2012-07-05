ef73b37e0a7b28b703ce113c0ac76aba8f2c0799
Victor Nicollet <victor@nicollet.net>
2012-07-05 13:07:05 +0000

    Avatar grid eval cleanup
    git-svn-id: svn://svn.runorg.com/app/retheme@7686 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/mAvatarGridEval.ml
+++ ocaml/mAvatarGridEval.ml
@@ -26,25 +26,3 @@ include Ohm.Fmt.Make(struct
 	]
     ]
 end)
-
-let from_generic iid gid = function
-  | `profile what -> begin match what with 
-      | `fullname  -> `Avatar  (iid,`Name)
-      | `firstname -> `Profile (iid,`Firstname)
-      | `lastname  -> `Profile (iid,`Lastname)
-      | `email     -> `Profile (iid,`Email)
-      | `birthdate -> `Profile (iid,`Birthdate)
-      | `city      -> `Profile (iid,`City)
-      | `address   -> `Profile (iid,`Address)
-      | `zipcode   -> `Profile (iid,`Zipcode)
-      | `country   -> `Profile (iid,`Country)
-      | `phone     -> `Profile (iid,`Phone)
-      | `cellphone -> `Profile (iid,`Cellphone)
-      | `gender    -> `Profile (iid,`Gender)
-  end 
-  | `join (_,what) -> `Group (gid, begin match what with 
-      | `state   -> `Status
-      | `date    -> `Date
-      | `inList  -> `InList
-      | `field n -> `Field n
-  end)
