7e8b4a0af1019e84c6f2edcf47726a914f05f156
Victor Nicollet <victor@nicollet.net>
2012-07-07 15:59:01 +0000

    Rendering list of groups
    git-svn-id: svn://svn.runorg.com/app/retheme@7700 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cGroups.ml
+++ ocaml/cGroups.ml
@@ -7,10 +7,42 @@ open BatPervasives
 let () = CClient.define UrlClient.Members.def_home begin fun access -> 
   O.Box.fill $ O.decay begin 
 
+    let! list = ohm $ MEntity.All.get_by_kind access `Group in
+
+    let! list = ohm $ Run.list_map begin fun entity -> 
+      let! name = ohm $ CEntityUtil.name entity in
+      let! count, isMember = ohm begin 	
+
+	let! ()     = true_or (return (None,false)) (not (MEntity.Get.draft entity)) in
+	let  gid    = MEntity.Get.group entity in 
+
+	let! status = ohm $ MMembership.status access gid in
+	let  mbr    = status = `Member in
+ 
+	let! group  = ohm_req_or (return (None,mbr)) $ MGroup.try_get access gid in
+	let! group  = ohm_req_or (return (None,mbr)) $ MGroup.Can.list group in
+	let  gid    = MGroup.Get.id group in 
+	let! count  = ohm $ MMembership.InGroup.count gid in
+
+	return (Some count # count,mbr) 
+
+      end in            
+      let status = MEntity.Get.status entity in
+      return (isMember, object
+	method id     = IEntity.to_string (MEntity.Get.id entity) 
+	method count  = count
+	method status = status 
+	method name   = name
+	method url    = "" 
+      end)
+    end list in 
+
+    let isMember, isNotMember = List.partition fst list in
+
     Asset_Group_List.render (object
       method create = None
-      method isMember = []
-      method isNotMember = []
+      method isMember = List.map snd isMember 
+      method isNotMember = List.map snd isNotMember
     end) 
   end
 end
