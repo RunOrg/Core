6eaccd4d0f5ccc9b8346428937653ad3e7d66fd2
Victor Nicollet <victor@nicollet.net>
2012-07-07 16:18:58 +0000

    Render individual group contents (empty so far)
    git-svn-id: svn://svn.runorg.com/app/retheme@7701 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cGroups.ml
+++ ocaml/cGroups.ml
@@ -4,7 +4,21 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+let contents access = 
+
+  let! eid = O.Box.parse IEntity.seg in
+  
+  O.Box.fill begin
+    Asset_Group_Page.render (object
+      method id = eid
+      method directory = return ignore
+    end)
+  end
+
 let () = CClient.define UrlClient.Members.def_home begin fun access -> 
+
+  let! contents = O.Box.add (contents access) in 
+
   O.Box.fill $ O.decay begin 
 
     let! list = ohm $ MEntity.All.get_by_kind access `Group in
@@ -33,16 +47,18 @@ let () = CClient.define UrlClient.Members.def_home begin fun access ->
 	method count  = count
 	method status = status 
 	method name   = name
-	method url    = "" 
+	method url    = Action.url UrlClient.Members.home (access # instance # key) 
+	  [ IEntity.to_string (MEntity.Get.id entity) ]  
       end)
     end list in 
 
     let isMember, isNotMember = List.partition fst list in
 
     Asset_Group_List.render (object
-      method create = None
-      method isMember = List.map snd isMember 
+      method create      = None
+      method isMember    = List.map snd isMember 
       method isNotMember = List.map snd isNotMember
+      method box         = O.Box.render contents 
     end) 
   end
 end
