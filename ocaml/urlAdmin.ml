d43f2c8001949a93314722a1b225c7623c112e6b
Victor Nicollet <victor@nicollet.net>
2012-08-02 07:53:15 +0000

    Add urls for instance and broadcast-count, make all admin urls secure
    git-svn-id: svn://svn.runorg.com/app/retheme@7891 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/urlAdmin.ml
+++ ocaml/urlAdmin.ml
@@ -6,5 +6,7 @@ open BatPervasives
 
 module A = Action.Args
 
-let home,   def_home   = O.declare O.core "admin" A.none
-let active, def_active = O.declare O.core "admin/active" (A.o A.int)
+let home,     def_home     = O.declare O.secure "admin" A.none
+let active,   def_active   = O.declare O.secure "admin/active" (A.o A.int)
+let public,   def_public   = O.declare O.secure "admin/public" (A.o A.int) 
+let instance, def_instance = O.declare O.secure "admin/public" (A.r IInstance.arg)
