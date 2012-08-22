deb6c731c068746b3df0f1c4d7cd65ea93a01710
Victor Nicollet <victor@nicollet.net>
2012-08-22 13:20:17 +0000

    JoinForm group page refactored
    git-svn-id: svn://svn.runorg.com/app/retheme@8021 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cGroups_admin_jForm.ml
+++ ocaml/cGroups_admin_jForm.ml
@@ -8,14 +8,16 @@ open CGroups_admin_common
 
 let () = define UrlClient.Members.def_jform begin fun parents entity access -> 
     
-  O.Box.fill begin 
+  let box body = 
+    O.Box.fill begin 
+      Asset_Admin_Page.render (object
+	method parents = [ parents # home ; parents # admin ] 
+	method here = parents # jform # title
+	method body = O.decay body
+      end)
+    end
+  in
+
+  box (return ignore) 
       
-    Asset_Admin_Page.render (object
-      method parents = [ parents # home ; parents # admin ] 
-      method here = parents # jform # title
-      method body = return ignore 
-    end)
-
-  end
-
 end
