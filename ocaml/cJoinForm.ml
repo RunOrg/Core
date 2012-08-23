e8e68cdacbefd5f242ddfb1a9a73fc74b4ac8c18
Victor Nicollet <victor@nicollet.net>
2012-08-23 10:12:30 +0000

    Render newly created fields
    git-svn-id: svn://svn.runorg.com/app/retheme@8034 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cJoinForm.ml
+++ ocaml/cJoinForm.ml
@@ -4,6 +4,19 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+module CreateFmt = Fmt.Make(struct
+  type json t = <
+    kind : [ `LongText 
+	   | `Textarea
+	   | `PickOne
+	   | `PickMany
+	   | `Date ] ;
+    text : string ;
+    req : bool ;
+    pick : string list ;
+  >
+end)
+
 let box access entity inner =
  
   let  gid   = MEntity.Get.group entity in 
@@ -15,6 +28,48 @@ let box access entity inner =
     ) list 
   in
 
+  let render_field field = 
+    let! label = ohm (TextOrAdlib.to_string (field # label)) in
+    return (object
+      method name     = field # name
+      method label    = label
+      method required = field # required
+      method edit     = match field # edit with 
+	| `LongText -> Asset_JoinForm_List_Longtext.render ()
+	| `Textarea -> Asset_JoinForm_List_Textarea.render ()
+	| `Date     -> Asset_JoinForm_List_Date.render ()
+	| `Checkbox -> Asset_JoinForm_List_Checkbox.render ()
+	| `PickOne list -> Asset_JoinForm_List_Pickone.render (choices list)
+	| `PickMany list -> Asset_JoinForm_List_Pickmany.render (choices list)
+    end)      
+  in
+
+  let! create = O.Box.react Fmt.Unit.fmt begin fun _ json _ res ->
+    
+    let! data = req_or (return res) $ CreateFmt.of_json_safe json in 
+    
+    let name = Id.gen () |> Id.str in
+
+    let field = object
+      method name     = name
+      method label    = `text data # text
+      method required = data # req
+      method edit     = let pick = List.map (fun t -> `text t) (data # pick) in
+			match data # kind with 
+			  | `LongText -> `LongText
+			  | `Textarea -> `Textarea
+			  | `Date -> `Date
+			  | `PickOne -> `PickOne pick
+			  | `PickMany -> `PickMany pick
+    end in
+
+    let! data = ohm $ render_field field in 
+    let! html = ohm $ Asset_JoinForm_List_Field.render data in
+
+    return $ Action.json [ "field", Html.to_json html ] res
+
+  end in 
+
   let render = 
 
     let! fields = ohm begin
@@ -22,26 +77,13 @@ let box access entity inner =
       return (MGroup.Fields.get group) 
     end in 
 
-    let! list = ohm $ Run.list_map begin fun field ->
-      let! label = ohm (TextOrAdlib.to_string (field # label)) in
-      return (object
-	method name     = field # name
-	method label    = label
-	method required = field # required
-	method edit     = match field # edit with 
-	  | `LongText -> Asset_JoinForm_List_Longtext.render ()
-	  | `Textarea -> Asset_JoinForm_List_Textarea.render ()
-	  | `Date     -> Asset_JoinForm_List_Date.render ()
-	  | `Checkbox -> Asset_JoinForm_List_Checkbox.render ()
-	  | `PickOne list -> Asset_JoinForm_List_Pickone.render (choices list)
-	  | `PickMany list -> Asset_JoinForm_List_Pickmany.render (choices list)
-      end)      
-    end fields in 
+    let! list = ohm $ Run.list_map render_field fields in 
 
     Asset_JoinForm_List.render (object
       method list = list 
       method form = (object
-	method submit = Json.String ""
+	method submit = JsCode.Endpoint.to_json 
+	  (OhmBox.reaction_endpoint create ())
       end)
     end)
 
