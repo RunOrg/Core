5204d5651c12a7ea5312a79d94e39cd3b8149fab
Victor Nicollet <victor@nicollet.net>
2012-08-27 15:02:50 +0000

    Remove colon to avoid newline
    git-svn-id: svn://svn.runorg.com/app/retheme@8087 b8684840-d642-4688-b1b8-d9d0b1097da4

--- assets/invite/fr.adlib.ml
+++ assets/invite/fr.adlib.ml
@@ -16,7 +16,7 @@ end
 | `Import_Help_FromExcel -> "Depuis un tableur"
 | `Import_Help_FromExcel_Detail -> "Créez trois colonnes email, prénom et nom, dans cet ordre, puis sélectionnez-les et copiez-collez leur contenu dans la case ci-contre." 
 | `Import_Help_FromBook -> "Depuis un carnet d'adresses"
-| `Import_Help_FromBook_Detail -> "Vous pouvez également utiliser le format :"
+| `Import_Help_FromBook_Detail -> "Vous pouvez également utiliser le format"
 | `Import_Help_FromBook_Example -> "Prénom Nom <adresse@email.com>" 
 | `Import_Help_Check -> "Validation préalable"
 | `Import_Help_Check_Detail -> "Vous pourrez vérifier et corriger la liste des membres avant leur inscription."
