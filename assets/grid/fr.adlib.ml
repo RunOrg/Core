798b3b68892430307278104cd7fbc4d706155777
Mehdi Foughali <mfoughali@runorg.com>
2012-08-27 14:54:04 +0000

    texte ajouter des membres dans les grilles
    git-svn-id: svn://svn.runorg.com/app/retheme@8085 b8684840-d642-4688-b1b8-d9d0b1097da4

--- assets/grid/fr.adlib.ml
+++ assets/grid/fr.adlib.ml
@@ -1,4 +1,4 @@
 | `Grid_Block_Wait -> "Nous préparons l'affichage de cette liste."
 | `Grid_Block_Wait_Thanks -> "Merci de votre patience"
 | `Grid_Block_Empty -> "Aucun élément à afficher"
-| `Grid_Link_Invite -> "Ajouter..."
+| `Grid_Link_Invite -> "Ajouter des membres"
