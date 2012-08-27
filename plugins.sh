91c8d66353d7464f2823bea208a290e3d09b4e68
Victor Nicollet <victor@nicollet.net>
2012-08-27 13:06:31 +0000

    Adjust to new plugins system
    git-svn-id: svn://svn.runorg.com/app/retheme@8065 b8684840-d642-4688-b1b8-d9d0b1097da4

---
+++ plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ohm plug ohmCouchProof ohmBox ohmForm ohmCouchVersioned ohmCouchTabular \
+         ohmSlug ohmAmazon ohmFacebook ohmText ohmAsset ohmSanitizeHtml \
+         ohmPaging ohmParseRss ohmCouchPollUrl ohmCouchPaypal \
+         ohmCouchUnique ohmArr 
