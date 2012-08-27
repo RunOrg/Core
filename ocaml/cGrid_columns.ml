2f30947c2ab6928f05ea6de0df50c173c96b5fa0
Victor Nicollet <victor@nicollet.net>
2012-08-27 15:38:18 +0000

    First rendering of grid column edit page
    git-svn-id: svn://svn.runorg.com/app/retheme@8091 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/cGrid_columns.ml
+++ ocaml/cGrid_columns.ml
@@ -4,6 +4,32 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+module Grid    = MAvatarGrid
+
 let box access entity render = 
   
-  render (return ignore) 
+  let fail = render (return ignore) in 
+
+  (* Extract the AvatarGrid identifier *)
+
+  let  draft  = MEntity.Get.draft entity in 
+
+  let  gid = MEntity.Get.group entity in
+  let! group = ohm $ O.decay (MGroup.try_get access gid) in
+  let! group = ohm $ O.decay (Run.opt_bind MGroup.Can.list group) in
+  let  group = if draft then None else group in   
+  let! group = req_or fail group in 
+
+  let  grid  = MGroup.Get.list group in 
+  let  lid   = Grid.list_id grid in
+  
+  let! columns = ohm $ O.decay (
+    let! columns, _, _ = ohm_req_or (return []) $ Grid.MyGrid.get_list lid in
+    return columns
+  ) in
+
+  let body = Asset_Grid_Edit.render (object
+    method columns = List.map MAvatarGridColumn.(fun c -> TextOrAdlib.to_string c.label) columns
+  end) in
+
+  render body
