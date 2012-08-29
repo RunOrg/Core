21f736e29d7d32ce051a8c13d2dad85e695ff5d4
Mehdi Foughali <mfoughali@runorg.com>
2012-08-29 09:26:17 +0000

    mise à jour catalog.ml
    git-svn-id: svn://svn.runorg.com/app/retheme@8118 b8684840-d642-4688-b1b8-d9d0b1097da4

--- splash/catalog.ml
+++ splash/catalog.ml
@@ -14,19 +14,43 @@ let pages = [
 
   page "" "RunOrg Associations - Configuration Standard"
     [ composite `LR
-	(pride ~title:"Configuration standard" "Description")
-	(create "Simple")
-    ] ;
-
-  page "/asso/Students" "RunOrg Associations - BDE et Associations Etudiantes"
-    [ pride ~title:"BDE & Etudiants" "Description" ] ;
+	(pride ~title:"Configuration standard" "Une configuration généraliste adaptée à toutes les associations")
+	(create "Simple") ;
 	
-  page "" "RunOrg Associations - Economie Sociale et Solidaire"
+      composite `LR
+	(price
+	   "Gratuit"
+	   "pour les associations"
+	   "de moins de 2000 adhérents") 
+	(bullets
+	   ~title:"La plateforme la plus complète pour les associations"
+	   ~subtitle:"Tous les outils utiles regroupés dans un même espace pour vous faire gagner du temps"
+	   ~ordered:false
+	   [ "Communication interne avec l'espace membre" ;
+	     "Communication externe avec le site Internet" ;
+	     "Outils de gestion et d'organisation collaboratifs" ;
+	     "Hébergement, mises à jour et maintenance informatique" ]
+	); 
+    ] ;
+	
+  page "/asso/Students" "RunOrg Associations - BDE et Associations Etudiantes"
+   [ composite `LR
+	(pride ~title:"Associations étudiantes" "Pour les BDE, les BDA, les BDS et toutes les associations étudiantes")
+	(create "students")
+    ] ;
+	
+  page "/asso/ESS" "RunOrg Associations - Economie Sociale et Solidaire"
     [ composite `LR
-	(pride ~title:"Economie Sociale et Solidaire" "Description")
+	(pride ~title:"Economie Sociale et Solidaire" "Pour toutes les associations de l'économie sociale et solidaire")
 	(create "ess")
     ] ;
 
+  page "/asso/Improvisation" "RunOrg Associations - Théâtre d'Improvisation"
+    [ composite `LR
+	(pride ~title:"Théâtre d'Improvisation" "Spécialement créé pour les troupes et les clubs pratiquant le théâtre d'improvisation")
+	(create "impro")
+    ] ;	
+	
   page "/ComiteEnt" "RunOrg Comités d'Entreprise"
     [ pride ~title:"Comités d'Entreprise" "Description" ]
         
