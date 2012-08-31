3c990279be062bc8f00fe0621c428f5747fa18b9
Victor Nicollet <victor@nicollet.net>
2012-08-31 08:24:17 +0000

    Install export plugin
    git-svn-id: svn://svn.runorg.com/app/retheme@8158 b8684840-d642-4688-b1b8-d9d0b1097da4

--- plugins.sh
+++ plugins.sh
@@ -2,4 +2,4 @@
 ohm plug ohmCouchProof ohmBox ohmForm ohmCouchVersioned ohmCouchTabular \
          ohmSlug ohmAmazon ohmFacebook ohmText ohmAsset ohmSanitizeHtml \
          ohmPaging ohmParseRss ohmCouchPollUrl ohmCouchPaypal \
-         ohmCouchUnique ohmArr 
+         ohmCouchUnique ohmArr ohmCouchExport
