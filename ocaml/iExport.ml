9aa65f85214fbc6f944e2fc310eef5ce8fabcae7
Victor Nicollet <victor@nicollet.net>
2012-08-31 09:27:50 +0000

    Readability proof for exports
    git-svn-id: svn://svn.runorg.com/app/retheme@8165 b8684840-d642-4688-b1b8-d9d0b1097da4

--- ocaml/iExport.ml
+++ ocaml/iExport.ml
@@ -1,9 +1,21 @@
 (* © 2012 RunOrg *) 
 
+open Ohm
+open Ohm.Universal
 open BatPervasives
 
 include Ohm.Id.Phantom
 
 module Assert = struct
   let read = identity 
+end
+
+module Deduce = struct
+
+  let make_read_token user item = 
+    ICurrentUser.prove "read_export" user [Id.str item]
+      
+  let from_read_token user item proof =
+    if ICurrentUser.is_proof proof "read_export" user [Id.str item] then Some item else None
+
 end
--- ocaml/iExport.mli
+++ ocaml/iExport.mli
@@ -5,3 +5,8 @@ include Ohm.Id.PHANTOM
 module Assert : sig
   val read : 'any id -> [`Read] id
 end
+
+module Deduce : sig
+  val make_read_token  : 'u ICurrentUser.id -> [`Read] id  -> string
+  val from_read_token  : 'u ICurrentUser.id -> 'any id     -> string -> [`Read] id option
+end
