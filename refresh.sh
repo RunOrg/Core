f0d1a14a8c67ec3e2ebd68d506b337bd50da92c4
Victor Nicollet <victor@nicollet.net>
2012-10-02 12:27:27 +0200

    Ping all servers after rebuilding

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 git remote update
 git status -uno | grep 'branch is behind' -q && git pull && ohm init . || echo "Code is up to date!"
+curl http://runorg.local > /dev/null
+curl http://dev.runorg.com > /dev/null
+curl http://runorg.com > /dev/null
