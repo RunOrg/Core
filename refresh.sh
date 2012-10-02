8bbf632cfc94d2d806d7afaf8a1be2cc3bf9d645
Victor Nicollet <victor@nicollet.net>
2012-10-02 14:01:22 +0200

    Correct refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git remote update
-git status -uno | grep 'branch is behind' -q && git pull && ohm init . || echo "Code is up to date!"
-curl http://runorg.local > /dev/null
-curl http://dev.runorg.com > /dev/null
-curl http://runorg.com > /dev/null
+PATH=/usr/local/bin:/usr/bin:/bin
+git remote update > /dev/null
+git status -uno | grep 'branch is behind' -q && git pull && ohm init .
+curl http://runorg.local -s > /dev/null
+curl http://dev.runorg.com -s > /dev/null
+curl http://runorg.com -s > /dev/null
