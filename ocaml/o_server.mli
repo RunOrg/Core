a1b2dae908f1286ff24f1e11ebcf514200b69693
Victor Nicollet <victor@nicollet.net>
2012-10-05 12:53:48 +0200

    Use new white key type

--- ocaml/o_server.mli
+++ ocaml/o_server.mli
@@ -2,4 +2,4 @@
 
 val core : string -> IWhite.t option Ohm.Action.server
 val secure : string -> IWhite.t option Ohm.Action.server
-val client : string -> (string * IWhite.t option) Ohm.Action.server
+val client : string -> IWhite.key Ohm.Action.server
