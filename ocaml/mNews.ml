86c751c5f3539ba727f04a4bcfc9b52bd29c1196
Victor Nicollet <victor@nicollet.net>
2012-10-08 08:34:07 +0200

    News lock implementation

--- ocaml/mNews.ml
+++ ocaml/mNews.ml
@@ -1,4 +1,8 @@
 (* © 2012 RunOrg *)
 
+open Ohm
+open Ohm.Universal
+open BatPervasives
 
+module Lock = MNews_lock
 
---
+++ ocaml/mNews_lock.ml
@@ -0,0 +1,56 @@
+(* © 2012 RunOrg *)
+
+open Ohm
+open Ohm.Universal
+open BatPervasives
+
+let lock_life = 60.
+let expire_time = 60.
+
+module Item = struct
+  module T = struct
+    type json t = {
+      time : float ;
+      lock : float option ;
+    }
+  end
+  include T 
+  include Fmt.Extend(T)
+
+  let lock   now doc = { doc with lock = Some now }
+  let unlock now doc = { time = now ; lock = None }
+    
+  let make now = { time = now ; lock = Some now } 
+
+end
+ 
+let locked now doc = match doc.Item.lock with 
+  | None -> true
+  | Some lock -> lock +. lock_life > now 
+
+let recent now doc = doc.Item.time +. expire_time > now
+
+include CouchDB.Convenience.Table(struct let db = O.db "news-lock" end)(IUser)(Item)
+
+let ensure id = 
+  let! now = ohmctx (#time) in
+  Tbl.ensure id (lazy (Item.make now))
+
+let get id = 
+  let! item = ohm_req_or (ensure id) (Tbl.get id) in
+  return item 
+
+let grab id = 
+  let! now = ohmctx (#time) in
+  Tbl.transact id begin fun item ->
+    let item = match item with None -> Item.make now | Some item -> item in 
+    let recent = recent now item and locked = locked now item in    
+    return (
+      (object method recent = recent method locked = locked end),
+      if locked then `keep else `put (Item.lock now item)
+    )
+  end 
+
+let release id = 
+  let! now = ohmctx (#time) in
+  Tbl.update id (Item.unlock now) 
