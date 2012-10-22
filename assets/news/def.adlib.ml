c8c0fbdb99e332426204392c0e062d1112aeb366
Victor Nicollet <victor@nicollet.net>
2012-10-22 14:37:27 +0200

    Render full welcome message

--- assets/news/def.adlib.ml
+++ assets/news/def.adlib.ml
@@ -1,1 +1,4 @@
 | `News_Welcome
+| `News_Welcome_Instances
+| `News_Welcome_Instances_Click
+| `News_Welcome_Create_Button
--- assets/news/fr.adlib.ml
+++ assets/news/fr.adlib.ml
@@ -1,1 +1,5 @@
 | `News_Welcome -> "Bienvenue !"
+| `News_Welcome_Instances -> "Vos espaces privés apparaissent à gauche de cette page."
+| `News_Welcome_Instances_Click -> "Cliquez dessus pour vous y rendre."
+| `News_Welcome_Create_Button -> "Créer un espace privé"
+
