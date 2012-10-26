b736d2efa087c4e147901db0fdc803b8e34c577f
Victor Nicollet <victor@nicollet.net>
2012-10-26 09:42:54 +0200

    DIsplay title, redirect if not correct unbound page

--- ocaml/cNetwork_unbound.ml
+++ ocaml/cNetwork_unbound.ml
@@ -7,11 +7,32 @@ open BatPervasives
 let () = UrlNetwork.def_unbound begin fun req res ->
 
   let uid = CSession.get req in
+  let iid = req # args in 
 
-  let html = Asset_Network_Unbound.render (object
-    method navbar = (req # server,uid,None)
-  end) in
+  let not_found = C404.render (req # server) uid res in
 
-  CPageLayout.core (req # server) `Network_Unbound html res
+  let! profile = ohm_req_or not_found (MInstance.Profile.get iid) in
+
+  if snd (profile # key) <> req # server then 
+
+    (* Wrong domain name : redirect *)
+    let url = Action.url (req # self) (snd (profile # key)) iid in
+    return (Action.redirect url res)
+
+  else if profile # unbound = None then
+    
+    (* Profile is bound : redirect *)
+    let url = Action.url UrlClient.website (profile # key) () in
+    return (Action.redirect url res)
+
+  else
+
+    (* Profile is unbound : render wait page *)
+    let html = Asset_Network_Unbound.render (object
+      method navbar = (req # server,uid,None)
+      method name   = profile # name
+    end) in
+    
+    CPageLayout.core (req # server) `Network_Unbound html res
 
 end
