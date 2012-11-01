d2febbb173bc9aa3d59bc03b3d5b2e4e2228fda2
Victor Nicollet <victor@nicollet.net>
2012-11-01 11:55:59 +0100

    Include splash CSS

--- ocaml/cPortal_FFBAD.ml
+++ ocaml/cPortal_FFBAD.ml
@@ -6,10 +6,12 @@ open BatPervasives
 
 let owid = Some ConfigWhite.ffbad
 
+let splash_css = "/FFBAD/splash.css"
+
 let render ?(css=[]) ?(js=[]) ?head ?favicon ?(body_classes=[]) ~title html = 
   Html.print_page
     ~js:(CPageLayout.js false @ js)
-    ~css:([Asset.css] @ CPageLayout.white_css owid @ css)
+    ~css:([Asset.css] @ CPageLayout.white_css owid @ [ splash_css ] @ css)
     ?head
     ~favicon:(ConfigWhite.favicon owid)
     ~title
