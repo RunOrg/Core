9b4218409e1d68ee4f61e181776cfd91d1e3ed66
Victor Nicollet <victor@nicollet.net>
2012-11-05 06:54:31 +0100

    User SQL export

--- ocaml/cAdmin_sqlize.ml
+++ ocaml/cAdmin_sqlize.ml
@@ -29,16 +29,23 @@ end
     
 let () = UrlAdmin.def_getSQL $ admin_only begin fun cuid req res ->
 
-  let step, id = req # args in
+  let step, id_opt = req # args in
 
-  let respond ?(cache=false) ?(count=0) next sql = 
+  let respond ?(count=0) next sql = 
     let args = (if next = None then step + 1 else step), next in
     return (Action.json [
       "count", Json.Int count ;
-      "cache", Json.Bool cache ;
+      "cache", Json.Bool (next <> None) ;
       "sql",   Json.String sql ;
       "url",   Json.String (Action.url (req # self) (req # server) args)
     ] res) 
+  in
+
+  let sql_str = Printf.sprintf "%S" in
+  let sql_optstr = function None -> "NULL" | Some s -> sql_str s in
+  let domain = function
+    | None -> "runorg.com"
+    | Some wid -> try (ConfigWhite.domain wid) with _ -> "[INCONNU]"
   in
 
   match step with 
@@ -51,7 +58,7 @@ CREATE TABLE user (
   usr_firstname VARCHAR(255), 
   usr_lastname VARCHAR(255), 
   usr_email VARCHAR(255) NOT NULL,
-  usr_white VARCHAR(255)
+  usr_white VARCHAR(255) NOT NULL
 ) ;
 
 DROP TABLE IF EXISTS instance ;
@@ -77,8 +84,19 @@ CREATE TABLE ins_usr (
   usr_id, usr_firstname, usr_lastname, usr_email, usr_white
 ) VALUES\n"
 
-    | 2 -> respond None ""
-
+    | 2 -> let  uid_opt = BatOption.map IUser.of_id id_opt in 
+	   let! users, uid_opt = ohm $ MUser.Backdoor.list ~count:10 uid_opt in
+ 	   let  count = List.length users in 
+	   let  id_opt = BatOption.map IUser.to_id uid_opt in  
+	   respond ~count id_opt (String.concat ",\n" (List.map begin fun (uid,user) ->
+	     Printf.sprintf "(%s,%s,%s,%s,%s)" 
+	       (sql_str (IUser.to_string uid))
+	       (sql_optstr (user # firstname))
+	       (sql_optstr (user # lastname)) 
+	       (sql_str (user # email)) 
+	       (sql_str (domain (user # white)))
+	   end users))
+	   
     | 3 -> respond None ";\n"
 
     (* Inserting instances ================================================================= *)
