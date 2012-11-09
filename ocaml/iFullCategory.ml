ce5334ddc9edd4e378b6fc6d58d8191e565d2e92
Victor Nicollet <victor@nicollet.net>
2012-11-09 11:06:45 +0100

    Remove unused files

--- ocaml/iFullCategory.ml
+++
@@ -1,8 +0,0 @@
-(* © 2012 RunOrg *)
-
-open Ohm
-
-include Fmt.Make(struct
-  module Nature = ICategory.Nature
-  type json t = ICategory.t * Nature.t option
-end)
--- ocaml/iFullCategory.mli
+++
@@ -1,3 +0,0 @@
-(* © 2012 RunOrg *)
-
-include Ohm.Fmt.FMT with type t = ICategory.t * ICategory.Nature.t option 
