69173a8e5ccbcd7d039b4395a146e2151b2a593a
Victor Nicollet <victor@nicollet.net>
2012-11-14 23:05:50 +0100

    Only show help bubble to admins

--- ocaml/cHelp_tooltip.ml
+++ ocaml/cHelp_tooltip.ml
@@ -4,6 +4,16 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
-let () = UrlClient.def_newhere begin fun req res ->
-  return (Action.json [ "ok", Json.Bool true ] res)
+let () = UrlClient.def_newhere $ CClient.action begin fun access req res ->
+
+  if req # get "mode" = Some "check" then
+  
+    let show = CAccess.admin access <> None in
+
+    return (Action.json [ "ok", Json.Bool show ] res)
+
+  else
+
+    return res
+
 end
