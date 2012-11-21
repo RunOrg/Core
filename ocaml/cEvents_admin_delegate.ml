71fe7f64bda53bce5a6e020bd57ba78a9099c213
Victor Nicollet <victor@nicollet.net>
2012-11-21 16:03:29 +0100

    Listing items (not plugged)

--- ocaml/cEvents_admin_delegate.ml
+++ ocaml/cEvents_admin_delegate.ml
@@ -13,9 +13,10 @@ let () = define UrlClient.Events.def_delegate begin fun parents entity access ->
     let! admin = ohm $ O.decay (MEntity.admin_group_name (access # iid)) in 
 
     let body = Asset_Delegate_List.render (object 
-      method kind   = `Event 
-      method admins = admin
-      method add    = None
+      method kind      = `Event 
+      method admins    = admin
+      method delegates = []
+      method add       = None
     end) in
 
     Asset_Admin_Page.render (object
