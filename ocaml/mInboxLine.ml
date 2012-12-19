a488913c649bc3f58f3bd94c4a836404fbfbc0c4
Victor Nicollet <victor@nicollet.net>
2012-12-19 18:10:01 +0100

    Common InboxLine model code

--- ocaml/mInboxLine.ml
+++ ocaml/mInboxLine.ml
@@ -4,6 +4,9 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+open MInboxLine_common
+
 let () = 
   let! eid = Sig.listen MEvent.Signals.on_bind_inboxLine in
+  let  ()  = Util.log "Binding inbox line for %s" (IEvent.to_string eid) in
   return () 
---
+++ ocaml/mInboxLine_common.ml
@@ -0,0 +1,64 @@
+(* © 2012 RunOrg *)
+
+open Ohm
+open Ohm.Universal
+open BatPervasives
+
+(* Information types --------------------------------------------------------------------------------------- *) 
+
+module Info = struct
+
+  module Album = struct
+    module T = struct
+      type json t = {
+	id : IAlbum.t ;      
+	n  : int ;
+      }
+    end
+    include T
+    include Fmt.Extend(T)
+  end
+    
+  module Folder = struct
+    module T = struct
+      type json t = {
+	id : IFolder.t ;      
+	n  : int ;
+      }
+    end
+    include T
+    include Fmt.Extend(T)
+  end
+    
+  module Wall = struct
+    module T = struct
+      type json t = {
+	id : IFeed.t ;      
+	n  : int ;
+      }
+    end
+    include T
+    include Fmt.Extend(T)
+  end
+
+end
+
+(* Actual inbox line type ----------------------------------------------------------------------------------- *)
+
+module Line = struct
+  module T = struct
+    type json t = {
+      owner  : IInboxLineOwner.t ;
+      album  : Info.Album.t option ;
+      folder : Info.Folder.t option ; 
+      wall   : Info.Wall.t option ;
+      time   : float ;
+    }
+  end
+  include T
+  include Fmt.Extend(T)
+end
+
+(* Database and table -------------------------------------------------------------------------------------- *)
+
+include CouchDB.Convenience.Table(struct let db = O.db "inbox-line" end)(IInboxLine)(Line)
