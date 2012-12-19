41ea4c824a210db7083850b3f0f65132d5643488
Victor Nicollet <victor@nicollet.net>
2012-12-19 18:29:43 +0100

    Split refresh into async procedure

--- ocaml/mInboxLine.ml
+++ ocaml/mInboxLine.ml
@@ -8,8 +8,13 @@ open MInboxLine_common
 
 module ByOwner = MInboxLine_byOwner
 
+let refresh = O.async # define "inbox-line-refresh" IInboxLine.fmt 
+  begin fun ilid -> 
+    let  ()  = Util.log "Refresh InboxLine %s" (IInboxLine.to_string ilid) in
+    return ()       
+  end
+
 let () = 
   let! eid  = Sig.listen MEvent.Signals.on_bind_inboxLine in
   let! ilid = ohm $ ByOwner.get_or_create (`Event eid) in 
-  let  ()  = Util.log "Binding inbox line for %s -> %s" (IEvent.to_string eid) (IInboxLine.to_string ilid) in
-  return () 
+  refresh ilid
