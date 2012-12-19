49f32f835686832da67907a60255c1039f478454
Victor Nicollet <victor@nicollet.net>
2012-12-19 13:19:20 +0100

    Feed satellite migration

--- ocaml/mFeed.ml
+++ ocaml/mFeed.ml
@@ -221,3 +221,21 @@ let bot_find iid own =
 
   return $ IFeed.Assert.bot id (* This is a bot-only access. *)
 
+(* {{MIGRATION}} *)
+
+let () = 
+  let! eid, evid, _ = Sig.listen MEntity.on_migrate in 
+  let! found = ohm_req_or (return ()) $ (ByOwnerView.doc (IEntity.to_id eid) |> Run.map Util.first) in
+  let  doc, id = found # doc, found # id in  
+  if doc # own <> Some (`Event evid) then
+
+    let changed = object
+      method t     = doc # t 
+      method iid   = doc # iid
+      method own   = Some (`Event evid)
+    end in 
+
+    let! _ = ohm $ Tbl.set (IFeed.of_id id) changed in
+    return () 
+    
+  else return () 
