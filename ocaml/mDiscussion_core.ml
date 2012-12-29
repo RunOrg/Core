78513bbb4635de8c57f62c413b90df5ba5b8930d
Victor Nicollet <victor@nicollet.net>
2012-12-29 01:49:47 +0100

    Implement MDiscussion.Core with HEntity.Core

--- ocaml/mDiscussion_core.ml
+++ ocaml/mDiscussion_core.ml
@@ -8,18 +8,6 @@ module Cfg = struct
 
   let name = "discussion"
 
-  module DataDB = struct
-    let database = O.db "discussion"
-    let host = "localhost"
-    let port = 5984
-  end
-
-  module VersionDB = struct
-    let database = O.db "discussion-v"
-    let host = "localhost"
-    let port = 5984
-  end
-
   module Id = IDiscussion
 
   module Diff = Fmt.Make(struct
@@ -47,13 +35,6 @@ module Cfg = struct
     include Fmt.Extend(T)
   end
 
-  type ctx = O.ctx
-
-  let couchDB ctx = (ctx : O.ctx :> CouchDB.ctx) 
-
-  module VersionData = MUpdateInfo
-  module ReflectedData = Fmt.Unit
-
   let do_apply t time = Data.(function
     | `SetTitle  title -> { t with title ; time }
     | `SetBody   body  -> { t with body ; time }
@@ -64,16 +45,18 @@ module Cfg = struct
   let apply diff = 
     return (fun _ time t -> return (do_apply t time diff))
 
-  let reflect _ _ = return () 
-
 end
 
-module Store = OhmCouchVersioned.Make(Cfg)
+type diff_t = Cfg.Diff.t 
+type data_t = Cfg.Data.t = {
+  iid   : IInstance.t ;
+  gids  : IGroup.t list ; 
+  title : string ; 
+  body  : MRich.OrText.t ;
+  time  : float ;
+  crea  : IAvatar.t ;
+  del   : IAvatar.t option ;
+}
 
-module Design = struct
-  module Database = Store.DataDB
-  let name = "discussion"
-end
-
-include Cfg.Data
+include HEntity.Core(Cfg)
 
