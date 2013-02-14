fe55374cac8af8cf33695b4dbf64ff3ad8e96a7d
Victor Nicollet <victor@nicollet.net>
2013-02-14 19:03:23 +0100

    [DMS] Document identifiers

--- ocaml/DMS.ml
+++ ocaml/DMS.ml
@@ -1,6 +1,7 @@
 (* © 2013 RunOrg *)
 
 module IRepository = DMS_IRepository
+module IDocument = DMS_IDocument
 module MRepository = DMS_MRepository
 module CRepository = DMS_CRepository
 module Url = DMS_Url
