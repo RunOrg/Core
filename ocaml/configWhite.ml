cf5b05c8c7c32d3714b3d5516fc02752a23f54bb
Victor Nicollet <victor@nicollet.net>
2013-02-15 13:59:36 +0100

    [M2014] White config

--- ocaml/configWhite.ml
+++ ocaml/configWhite.ml
@@ -1,15 +1,21 @@
 (* © 2012 RunOrg *)
 
-let test  = IWhite.of_string "test"
-let ffbad = IWhite.of_string "ffbad"
-let fscf  = IWhite.of_string "fscf"
-let innov = IWhite.of_string "innov"
+let test   = IWhite.of_string "test"
+let ffbad  = IWhite.of_string "ffbad"
+let fscf   = IWhite.of_string "fscf"
+let innov  = IWhite.of_string "innov"
+let m2014  = IWhite.of_string "m2014"
+let clichy = IWhite.of_string "clichy"
+let alfort = IWhite.of_string "alfort"
 
 let all = [
   test ;
   ffbad ; 
   fscf ;
   innov ;
+  m2014 ;
+  clichy ; 
+  alfort ; 
 ]
 
 type t = 
@@ -17,6 +23,9 @@ type t =
   | `Test
   | `FFBAD
   | `FSCF
+  | `M2014
+  | `Clichy
+  | `Alfort 
   | `Innov
   ]
 
@@ -26,7 +35,10 @@ let represent = function
       | "test" -> `Test 
       | "ffbad" -> `FFBAD
       | "fscf" -> `FSCF
+      | "m2014" -> `M2014
       | "innov" -> `Innov
+      | "clichy" -> `Clichy
+      | "alfort" -> `Alfort 
       | other -> let error = "Unknown white id #" ^ other in
 		 Ohm.Util.log "%s" error ;
 		 raise Not_found
@@ -36,13 +48,19 @@ let domain id = match represent (Some id) with
   | `Test -> "test.local" 
   | `FFBAD -> "ffbad.fr"
   | `FSCF -> "lafscf.fr"
+  | `M2014 -> "m2014.fr"
   | `Innov -> "my-innovation.org" 
+  | `Clichy -> "clichy.fr"
+  | `Alfort -> "alfort.fr"
 
 let white = function 
   | "test.local" -> Some test
   | "ffbad.fr" -> Some ffbad
   | "lafscf.fr" -> Some fscf
+  | "m2014.fr" -> Some m2014
   | "my-innovation.org" -> Some innov
+  | "clichy.fr" -> Some clichy
+  | "alfort.fr" -> Some alfort 
   | _ -> None
 
 let slice_domain domain = 
@@ -61,6 +79,9 @@ let name id = match represent id with
   | `Test   -> "Fédération de Test"
   | `FFBAD  -> "Fédération Française de Badminton"
   | `FSCF   -> "Fédération Sportive et Culturelle de France"
+  | `M2014  -> "M2014.fr"
+  | `Clichy -> "Clichy"
+  | `Alfort -> "Alfortville"
   | `Innov  -> "My Innovation"
 
 let the id = match represent id with 
@@ -68,6 +89,9 @@ let the id = match represent id with
   | `Test   -> "la Fédération de Test"
   | `FFBAD  -> "la Fédération Française de Badminton"
   | `FSCF   -> "la Fédération Sportive et Culturelle de France"
+  | `M2014  -> "M2014.fr" 
+  | `Clichy -> "Clichy"
+  | `Alfort -> "Alfortville"
   | `Innov  -> "My Innovation"
 
 let of_the id = match represent id with 
@@ -75,6 +99,9 @@ let of_the id = match represent id with
   | `Test   -> "de la Fédération de Test"
   | `FFBAD  -> "de la Fédération Française de Badminton"
   | `FSCF   -> "de la Fédération Sportive et Culturelle de France"
+  | `M2014  -> "de M2014.fr"
+  | `Clichy -> "de Clichy"
+  | `Alfort -> "d'Alfortville"
   | `Innov  -> "de My Innovation"
 
 let email id = match represent id with 
@@ -82,6 +109,9 @@ let email id = match represent id with
   | `Test   -> "contact+test@runorg.com"
   | `FFBAD  -> "contact@ffbad.fr" (* TODO: find an address *) 
   | `FSCF   -> "contact@lafscf.fr" (* TODO: find an address *)
+  | `M2014  -> "contact@m2014.fr"
+  | `Clichy -> "contact@clichy.fr" (* TODO: find an address *)
+  | `Alfort -> "contact@alfort.fr" (* TODO: find an address *) 
   | `Innov  -> "contact@my-innovation.com" (* TODO : find an address *)
 
 let no_reply id = match represent id with
@@ -89,6 +119,9 @@ let no_reply id = match represent id with
   | `Test   -> "no-reply+test@runorg.com"
   | `FFBAD  -> "no-reply@ffbad.fr" 
   | `FSCF   -> "no-reply@lafscf.fr"
+  | `M2014  -> "no-reply@m2014.fr"
+  | `Clichy -> "no-reply@clichy.fr"
+  | `Alfort -> "no-reply@alfort.fr"
   | `Innov  -> "no-reply@my-innovation.com" 
 
 let short id = match represent id with 
@@ -96,6 +129,9 @@ let short id = match represent id with
   | `Test   -> "FdT"
   | `FFBAD  -> "FFBAD"
   | `FSCF   -> "FSCF"
+  | `M2014  -> "M2014"
+  | `Clichy -> "Clichy"
+  | `Alfort -> "Alfortville"
   | `Innov  -> "My Innovation"
 
 let favicon id = match represent id with
@@ -103,11 +139,17 @@ let favicon id = match represent id with
   | `Test   -> "/ffbad-favicon.ico"
   | `FFBAD  -> "/ffbad-favicon.ico"
   | `FSCF   -> "/fscf-favicon.ico"
+  | `M2014  -> "/m2014-favicon.ico"
   | `Innov  -> "/myInnovation-favicon.ico"
+  | `Clichy -> "/clichy-favicon.ico"
+  | `Alfort -> "/alfort-favicon.ico"
 
 let default_vertical id = match represent id with 
   | `RunOrg -> `Simple
   | `Test   -> `Simple
   | `FFBAD  -> `Badminton
+  | `M2014  -> `Campaigns
+  | `Clichy -> `Simple
+  | `Alfort -> `Simple
   | `FSCF   -> `Simple (* TODO: find a vertical *)
   | `Innov  -> `Simple (* TODO: find a vertical *)
