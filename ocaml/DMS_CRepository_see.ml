1e3d38e8c42c97d33067505e6b6c0479c1d958f5
Victor Nicollet <victor@nicollet.net>
2013-02-22 17:42:57 +0100

    [DMS] Repository contents stub

--- ocaml/DMS_CRepository_see.ml
+++ ocaml/DMS_CRepository_see.ml
@@ -1,1 +1,24 @@
 (* © 2013 RunOrg *)
+
+open Ohm
+open Ohm.Universal
+open BatPervasives
+
+module IRepository = DMS_IRepository
+module MRepository = DMS_MRepository
+module Url = DMS_Url
+
+let () = CClient.define Url.def_see begin fun access ->
+  
+  let  e404 = O.Box.fill (Asset_Client_PageNotFound.render ()) in
+
+  let  actor = access # actor in 
+  let! rid = O.Box.parse IRepository.seg in
+
+  let! repo = ohm_req_or e404 $ MRepository.view ~actor rid in
+
+  O.Box.fill begin 
+    return (Html.esc (MRepository.Get.name repo))
+  end 
+
+end 
