4578df32e4e3ab6f8e20236fe200084225d802ab
Victor Nicollet <victor@nicollet.net>
2013-02-22 18:26:48 +0100

    [DMS] Render files

--- ocaml/DMS_CRepository_see.ml
+++ ocaml/DMS_CRepository_see.ml
@@ -4,9 +4,34 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
-module IRepository = DMS_IRepository
-module MRepository = DMS_MRepository
-module Url = DMS_Url
+open DMS_CRepository_common 
+
+let render_files ?start ~count access repo self = 
+
+  let! time = ohmctx (#time) in
+
+  let  rid = MRepository.Get.id repo in 
+  let! files, next = ohm $ MDocument.All.in_repository ~actor:(access # actor) ?start ~count rid in
+
+  let  more = BatOption.map (fun next -> OhmBox.reaction_endpoint self next, Json.Null) next in 
+
+  let! items = ohm $ Run.list_map begin fun file -> 
+    let! author = ohm $ O.decay (CAvatar.mini_profile (snd (file # update))) in
+    return (object
+      method name    = file # name
+      method version = file # version 
+      method time    = (time, fst (file # update))
+      method author  = author
+      method url     = Action.url Url.file (access # instance # key)
+	[ IDocument.to_string (MDocument.Get.id (file # doc)) ]
+     end)
+  end files in
+
+  Asset_DMS_Repository_Inner.render (object
+    method items = items
+    method more  = more
+  end) 
+
 
 let () = CClient.define Url.def_see begin fun access ->
   
@@ -17,9 +42,15 @@ let () = CClient.define Url.def_see begin fun access ->
 
   let! repo = ohm_req_or e404 $ MRepository.view ~actor rid in
 
+  let! more = O.Box.react Fmt.Float.fmt begin fun start _ self res -> 
+    let! html = ohm $ render_files ~start ~count:8 access repo self in
+    return $ Action.json [ "more", Html.to_json html ] res
+  end in 
+
   O.Box.fill begin 
     Asset_DMS_Repository.render (object
       method name   = MRepository.Get.name repo 
+      method list   = render_files ~count:0 access repo more 
     end)
   end 
 
--- ocaml/DMS_Url.ml
+++ ocaml/DMS_Url.ml
@@ -2,8 +2,9 @@
 
 open UrlClient.Common
 
-let home, def_home = root "dms/repos"
-let () = VNavbar.registerPlugin `DMS home `DMS_Navbar
-
+let home,   def_home   = root             "dms/repos"
 let create, def_create = child def_home   "dms/repo/create"
 let see,    def_see    = child def_create "dms/repo/view"
+let file,   def_file   = child def_see    "dms/file"
+
+let () = VNavbar.registerPlugin `DMS home `DMS_Navbar
