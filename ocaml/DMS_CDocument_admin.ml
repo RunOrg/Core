821163eb5880078cc63cc5e3085d9f903591080d
Victor Nicollet <victor@nicollet.net>
2013-02-28 14:39:56 +0100

    [DMS] Conditional display of removal link

--- ocaml/DMS_CDocument_admin.ml
+++ ocaml/DMS_CDocument_admin.ml
@@ -14,30 +14,36 @@ module Share  = DMS_CDocument_admin_share
 let () = define Url.Doc.def_admin begin fun parents rid doc access ->
   O.Box.fill begin 
 
-    let choices = Asset_Admin_Choice.render [
+    let! can_remove = ohm begin 
+      let! repo = ohm_req_or (return false) (MRepository.get ~actor:(access # actor) rid) in
+      let!  _   = ohm_req_or (return false) (MRepository.Can.remove repo) in
+      return true
+    end in 
 
-      (object
+    let choices = Asset_Admin_Choice.render (BatList.filter_map identity [
+
+      Some (object
 	method img = VIcon.Large.page_edit
 	method url = parents # edit # url
 	method title = AdLib.get `DMS_Document_Edit_Link
 	method subtitle = Some (AdLib.get `DMS_Document_Edit_Sub)
        end) ;
 
-      (object
+      Some (object
 	method img = VIcon.Large.arrow_branch
 	method url = parents # share # url
 	method title = AdLib.get `DMS_Document_Share_Link
 	method subtitle = Some (AdLib.get `DMS_Document_Share_Sub)
        end) ;
 
-      (object
+      if can_remove then Some (object
 	method img = VIcon.Large.cross
 	method url = parents # delete # url
 	method title = AdLib.get `DMS_Document_Delete_Link
 	method subtitle = Some (AdLib.get `DMS_Document_Delete_Sub)
-       end) ;
+       end) else None ;
 
-    ] in
+    ]) in
 
     Asset_Admin_Page.render (object
       method parents = [ parents # home ]
