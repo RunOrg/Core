37a4c30f602d6f9740eec55d62ea9bc4980c35f3
Victor Nicollet <victor@nicollet.net>
2013-03-01 14:59:05 +0100

    [DMS] Plug in metadata form fields

--- ocaml/DMS_CDocument_admin_edit.ml
+++ ocaml/DMS_CDocument_admin_edit.ml
@@ -7,6 +7,74 @@ open BatPervasives
 open DMS_CDocument_common 
 open DMS_CDocument_admin_common
 
+(* Render an individual metafield *)
+let show_metafield ~fieldkey ~fieldinfo = 
+
+  let label = AdLib.get (fieldinfo # label) in
+
+  let seed (_,data) = try BatPMap.find fieldkey data with Not_found -> Json.Null in
+
+  (* String fields *)
+
+  let seed_string s = 
+    match seed s with 
+      | Json.String s -> return s
+      | _ -> return ""
+  in
+
+  let parse_string _ s = 
+    let s = BatString.trim s in
+    if s = "" then return (Ok Json.Null) else return (Ok (Json.String s))
+  in
+
+  (* Date fields *)
+
+  let seed_date s = 
+    match Date.of_json_safe (seed s) with 
+      | Some d -> return (Date.to_iso8601 d)
+      | None -> return ""
+  in
+  
+  let parse_date _ s = 
+    match Date.of_iso8601 s with 
+      | Some d -> return (Ok (Date.to_json d))
+      | None -> return (Ok (Json.Null))
+  in
+
+  (* Pickers *)
+
+  let format = Fmt.String.fmt in
+
+  let source list = 
+    List.map (fun (k,v) -> k, AdLib.write v) list
+  in
+
+  let seed_pickone s = 
+    match try Json.to_list (Json.to_string) (seed s) with _ -> [] with
+      | h :: _ -> return (Some h)
+      | [] -> return None
+  in
+
+  let seed_pickmany s = 
+    return (try Json.to_list (Json.to_string) (seed s) with _ -> [])
+  in
+
+  let parse_pickone _ = function
+    | Some s -> return (Ok (Json.String s))
+    | None -> return (Ok Json.Null)
+  in
+
+  let parse_pickmany _ l = 
+    return (Ok (Json.of_list Json.of_string l))
+  in
+
+  match fieldinfo # kind with 
+    | `Date       -> VEliteForm.date ~label seed_date parse_date
+    | `TextShort  -> VEliteForm.text ~label seed_string parse_string
+    | `TextLong   -> VEliteForm.textarea ~label seed_string parse_string
+    | `PickOne  l -> VEliteForm.radio ~label ~format ~source:(source l) seed_pickone parse_pickone
+    | `PickMany l -> VEliteForm.checkboxes ~label ~format ~source:(source l) seed_pickmany parse_pickmany
+
 let template metafields = 
 
   let inner = 
@@ -18,7 +86,7 @@ let template metafields =
     |> OhmForm.append (fun f name -> return $ f ~name) 
 	(VEliteForm.text
 	   ~label:(AdLib.get `DMS_Document_Edit_Name)
-	   (fun doc -> return (MDocument.Get.name doc)) 
+	   (fun (doc,_) -> return (MDocument.Get.name doc)) 
 	   (OhmForm.required (AdLib.get `DMS_Document_Edit_Required)))
 	
     |> OhmForm.append (fun f meta -> return $ f ~meta) begin
@@ -27,17 +95,8 @@ let template metafields =
       List.fold_left begin fun form (fieldkey, fieldinfo) -> 
 
 	(* For each field, append the result to the complete meta-map *)
-	OhmForm.append (fun map json -> return (BatPMap.add fieldkey json map)) begin 
-
-	  let label = AdLib.get (fieldinfo # label) in
-
-	  (* Determine what the field should look like dependeing on its kind. *)
-	  VEliteForm.text
-	    ~label
-	    (fun _ -> return "")
-	    (fun field string -> return (Ok (Json.String string)))
-
-	end form 
+	OhmForm.append (fun map json -> return (BatPMap.add fieldkey json map)) 
+	  (show_metafield ~fieldkey ~fieldinfo) form
 
       end (OhmForm.begin_object BatPMap.empty) metafields
       
@@ -51,6 +110,8 @@ let template metafields =
 let () = define Url.Doc.def_edit begin fun parents rid doc access ->
   
   let! metafields = ohm $ MDocMeta.fields (access # iid) in
+
+  let! meta = ohm $ MDocMeta.get (MDocument.Get.id doc) in
 
   let! save = O.Box.react Fmt.Unit.fmt begin fun _ json _ res -> 
     
@@ -71,8 +132,10 @@ let () = define Url.Doc.def_edit begin fun parents rid doc access ->
     (* Save the changes to the database *)
 
     let name = BatString.strip result # name in
-
     let! () = ohm $ MDocument.Set.name name doc (access # actor) in
+
+    let data = result # meta in
+    let! () = ohm $ MDocMeta.Set.data data meta (access # actor) in 
 
     (* Redirect to main page *)
 
@@ -84,7 +147,8 @@ let () = define Url.Doc.def_edit begin fun parents rid doc access ->
   O.Box.fill begin 
       
     let template = template metafields in
-    let form = OhmForm.create ~template ~source:(OhmForm.from_seed doc) in
+    let form = OhmForm.create ~template 
+      ~source:(OhmForm.from_seed (doc,MDocMeta.Get.data meta)) in
     let url  = OhmBox.reaction_endpoint save () in
         
     Asset_Admin_Page.render (object
