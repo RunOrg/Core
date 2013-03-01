b868baeeff88d121a7884f8abc42a675b8fed7c9
Victor Nicollet <victor@nicollet.net>
2013-03-01 10:35:53 +0100

    [DMS] Document meta-data model

---
+++ ocaml/DMS_MDocMeta.ml
@@ -0,0 +1,98 @@
+(* © 2013 RunOrg *)
+
+open Ohm
+open Ohm.Universal
+open BatPervasives
+
+module Field = struct
+  type t = string
+  let to_string = identity
+  let of_string = identity
+end
+
+module Data = Fmt.Make(struct
+  type t = (Field.t,Json.t) BatPMap.t
+  let json_of_t t = 
+    Json.Object (BatPMap.foldi (fun k v l -> (k,v) :: l) t [])
+  let t_of_json json = 
+    List.fold_left (fun m (k,v) -> BatPMap.add k v m) BatPMap.empty 
+      (Json.to_assoc json) 
+end)
+
+let clean m = 
+  BatPMap.filter ((<>) Json.Null) m 
+
+module Cfg = struct
+
+  let name = "dms-docmeta"
+
+  module DataDB = struct
+    let database = O.db name
+    let host     = "localhost"
+    let port     = 5984
+  end
+
+  module VersionDB = struct
+    let database = O.db (name ^ "-v") 
+    let host     = "localhost"
+    let port     = 5984
+  end
+
+  type ctx = O.ctx
+  let couchDB ctx = (ctx : O.ctx :> CouchDB.ctx) 
+
+  module Id = DMS_IDocument
+  module Data = Data
+  module Diff = Data 
+
+  let apply diff = return begin fun _ _ data ->
+    return (clean (BatPMap.foldi BatPMap.add data diff))
+  end
+
+  module VersionData = MUpdateInfo
+  module ReflectedData = Fmt.Unit
+  let reflect _ _ = return () 
+
+end
+
+module Store = OhmCouchVersioned.Make(Cfg)
+
+type 'relation t = {
+  id     : 'relation DMS_IDocument.id ;
+  exists : bool ;
+  data   : Data.t ;
+}
+
+module Get = struct
+  let id   t = t.id
+  let data t = t.data
+end
+
+let empty id = 
+  { id ; exists = false ; data = BatPMap.empty }
+
+module Set = struct
+  let data data t actor = 
+    O.decay begin 
+      let info = MUpdateInfo.self (MActor.avatar actor) in
+      let id = DMS_IDocument.decay t.id in 
+      if t.exists then 
+	let diff = 
+	  BatPMap.filteri 
+	    (fun k v -> v <> (try BatPMap.find k t.data with Not_found -> Json.Null)) 
+	    data 
+	in 
+	let! _ = ohm $ Store.update ~id ~diffs:[diff] ~info () in 
+	return () 
+      else
+	let! _ = ohm $ Store.create ~id ~init:BatPMap.empty ~diffs:[data] ~info () in
+	return ()
+    end 
+end
+
+let get id = 
+  O.decay begin 
+    let! found = ohm_req_or (return (empty id)) (Store.get (DMS_IDocument.decay id)) in
+    let  data = Store.current found in 
+    return { id ; exists = true ; data }
+  end 
---
+++ ocaml/DMS_MDocMeta.mli
@@ -0,0 +1,24 @@
+(* © 2013 RunOrg *)
+
+type 'relation t
+
+module Field : sig
+  type t 
+  val to_string : t -> string
+  val of_string : string -> t
+end
+
+module Data : sig
+  type t = (Field.t,Ohm.Json.t) BatPMap.t
+end
+
+module Get : sig
+  val id   : 'any t -> 'any DMS_IDocument.id 
+  val data : [<`View|`Admin] t -> Data.t 
+end
+
+module Set : sig
+  val data : Data.t -> [`Admin] t -> 'any MActor.t -> (#O.ctx, unit) Ohm.Run.t
+end 
+
+val get : 'rel DMS_IDocument.id -> (#O.ctx, 'rel t) Ohm.Run.t
