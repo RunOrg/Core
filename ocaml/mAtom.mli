1ae294155afd62d78bc28fb92a025f5383563a95
Victor Nicollet <victor@nicollet.net>
2013-03-21 15:52:16 +0100

    [Atom] Model and search

---
+++ ocaml/mAtom.mli
@@ -0,0 +1,23 @@
+(* © 2013 RunOrg *)
+
+type t = <
+  id     : IAtom.t ; 
+  nature : IAtom.Nature.t ;
+  label  : string ;
+> ;;
+
+val create : 'any MActor.t -> IAtom.Nature.t -> string -> (#O.ctx,IAtom.t) Ohm.Run.t
+
+module All : sig
+
+  val suggest : 
+       IInstance.t 
+    -> ?nature:IAtom.Nature.t
+    -> count:int
+    -> string 
+    -> (#O.ctx, t list) Ohm.Run.t
+
+end
+
+val get : actor:'any MActor.t -> IAtom.t -> (#O.ctx,t option) Ohm.Run.t
+
