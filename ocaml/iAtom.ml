b42afaba8c5389908efecdcf1da1503bcc52311b
Victor Nicollet <victor@nicollet.net>
2013-03-21 11:52:58 +0100

    [Atom] Identifier

---
+++ ocaml/iAtom.ml
@@ -0,0 +1,5 @@
+(* © 2013 RunOrg *)
+
+module Nature = PreConfig_Atom.Id
+
+include Ohm.Id.Phantom
---
+++ ocaml/iAtom.mli
@@ -0,0 +1,5 @@
+(* © 2013 RunOrg *)
+
+module Nature : Ohm.Fmt.FMT with type t = PreConfig_Atom.Id.t
+
+include Ohm.Id.PHANTOM
