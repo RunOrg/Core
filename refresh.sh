abe41861a2bd76f68fb1e85e31170ed4705ee6b4
Victor Nicollet <victor@nicollet.net>
2013-04-05 08:56:27 +0200

    [Bug] Fixes #156

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/bin:/bin
 git remote update > /dev/null
-git status -uno | grep 'branch is behind' -q && git pull && git submodule update && ohm init .
+git status -uno | grep 'branch is behind' -q && git pull && git submodule init && git submodule update && ohm init .
 curl http://runorg.com -s > /dev/null
