db24547b2d0e66bc4ca1f3d87ef7ccad6d1df75b
Victor Nicollet <victor@nicollet.net>
2013-04-05 08:22:53 +0200

    [Deploy] Use submodules

--- refresh.sh
+++ refresh.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/bin:/bin
-portals/FFBAD/refresh.sh
-portals/FSCF/refresh.sh
-portals/MyInnovation/refresh.sh
-portals/AssoHelp/refresh.sh
-portals/M2014/refresh.sh
-portals/Alfortville/refresh.sh
 git remote update > /dev/null
-git status -uno | grep 'branch is behind' -q && git pull && ohm init .
-curl http://runorg.local -s > /dev/null
+git status -uno | grep 'branch is behind' -q && git pull && git submodule update && ohm init .
 curl http://runorg.com -s > /dev/null
