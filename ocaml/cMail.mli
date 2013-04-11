8870e9aad2134cdb57349854b75ee0b2d05325e7
Victor Nicollet <victor@nicollet.net>
2013-04-11 20:52:56 +0200

    [Fix] Unclean dependency

--- ocaml/cMail.mli
+++ ocaml/cMail.mli
@@ -1,16 +1,5 @@
 (* © 2013 RunOrg *)
 
-module Wrap : sig
-
-  val render :
-       ?iid:IInstance.t
-    -> IWhite.t option
-    -> [`IsSelf] IUser.id
-    -> Ohm.Html.writer O.run
-    -> (string option * Ohm.Html.writer O.run) O.run
-
-end
-
 module Footer : sig
 
   val core : IMail.t -> [`IsSelf] IUser.id -> IWhite.t option -> VMailBrick.footer
