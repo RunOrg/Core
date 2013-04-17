d0dc7e54af5e44b1804562cd75cef09497a97689
Victor Nicollet <victor@nicollet.net>
2013-04-17 17:27:58 +0200

    [File] Registering a file

--- ocaml/mFile_ops.ml
+++ ocaml/mFile_ops.ml
@@ -4,6 +4,30 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+module Handle = MFile_handle
+module Store = MFile_store
+
+module Data = struct
+  module T = struct
+    type json t = {
+      owner  : [`User "u" of IUser.t | `Instance "i" of IInstance.t] option ;
+      author : [`User "u" of IUser.t | `Avatar "a" of IAvatar.t] option ; 
+      name   : string ;
+      size   : float ; 
+      store  : IFile.Storage.t ;
+      storeT : Ohm.Json.t ;
+      fileT  : Ohm.Json.t ; 
+      del    : float option ;
+      time   : float ; 
+      delok  : bool ; 
+    }
+  end
+  include T
+  include Fmt.Extend(T)
+end
+
+include CouchDB.Convenience.Table(struct let db = O.db "file-log" end)(Id)(Data)
+
 let delete h = 
   return () 
 
@@ -14,4 +38,25 @@ let upload store author ~public ~filename local =
   return None
 
 let register h author ~size ~filename = 
-  return () 
+
+  let! time = ohmctx (#time) in
+
+  let owner =  
+    let! provider = req_or None (Handle.provider h) in
+    Some (provider # owner)
+  in
+
+  let data = Data.({
+    owner ;
+    author ;
+    name = filename ;
+    size ;
+    store = h.Handle.store ;
+    storeT = h.Handle.storeT ;
+    fileT = h.Handle.fileT ;
+    del = (if author = None || owner = None then Some time else None) ;
+    time ;
+    delok = false ;
+  }) in
+
+  Tbl.set h.Handle.id data
