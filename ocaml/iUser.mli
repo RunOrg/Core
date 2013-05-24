6cb13a1fd69444ec5ff510681379ab01002e8bd6
Victor Nicollet <victor@nicollet.net>
2013-05-24 14:20:56 +0200

    [API] User obliteration

--- ocaml/iUser.mli
+++ ocaml/iUser.mli
@@ -10,6 +10,7 @@ module Assert : sig
   val is_new      : 'unknown id -> [`New] ICurrentUser.id 
   val is_old      : 'unknown id -> [`Old] ICurrentUser.id
   val confirm     : 'unknown id -> [`Confirm] id
+  val unsubscribe : 'unknown id -> [`Unsubscribe] id
 end
   
 module Deduce : sig
