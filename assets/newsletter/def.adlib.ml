5990ba61bdd239d6d9a4198ea52bd30b3f5e7aae
Victor Nicollet <victor@nicollet.net>
2013-06-26 14:02:37 +0200

    [Newsletter] Skin display page

--- assets/newsletter/def.adlib.ml
+++ assets/newsletter/def.adlib.ml
@@ -3,3 +3,5 @@
 | `Newsletter_Field_Title 
 | `Newsletter_Field_Body 
 | `Newsletter_Field_Required 
+
+| `Newsletter_Admin 
--- assets/newsletter/fr.adlib.ml
+++ assets/newsletter/fr.adlib.ml
@@ -3,3 +3,5 @@
 | `Newsletter_Field_Title -> "Titre"
 | `Newsletter_Field_Body -> "Contenu"
 | `Newsletter_Field_Required -> "Champ obligatoire"
+
+| `Newsletter_Admin -> "Modifier"
