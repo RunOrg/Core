0e3147a582c652fadac2709787491e365df04392
Victor Nicollet <victor@nicollet.net>
2013-06-26 13:09:31 +0200

    [Newsletter] Creation form

--- assets/newsletter/fr.adlib.ml
+++ assets/newsletter/fr.adlib.ml
@@ -1,5 +1,5 @@
+| `Newsletter_Create_Title -> "Composer une Newsletter"
 | `Newsletter_Save -> "Enregistrer"
-
 | `Newsletter_Field_Title -> "Titre"
 | `Newsletter_Field_Body -> "Contenu"
 | `Newsletter_Field_Required -> "Champ obligatoire"
--- ocaml/cNewsletter.ml
+++ ocaml/cNewsletter.ml
@@ -4,4 +4,5 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+module Create = CNewsletter_create
 module Edit = CNewsletter_edit
